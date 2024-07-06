0eebc7d6469d1cd004be2d026ff54629a7263015
Oisin Forde <oisin05forde@gmail.com>
2024-07-06 17:49:05 +1000

    Create templates for login and registeration pages, connected all pages approiately

--- server/app.py
+++ server/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 import os
 
+
 app = Flask(__name__,static_folder=os.path.abspath('clientend'),template_folder=os.path.abspath('clientend'))
 
 @app.route('/')
 def home():
     return render_template('landingPage/index.html')
+
+@app.route('/login')
+def login():
+    return render_template('loginPage/index.html')
+
+@app.route('/register')
+def register():
+    return render_template('registerPage/index.html')
 
 
 
